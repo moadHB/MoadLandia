d4a73fffcd82e78e4ff4f32b6963fefe54b6baa7
moadHB <moadhb@gmail.com>
2021-05-07 19:09:20 +0200

    finish background

--- js/app.js
+++ js/app.js
@@ -11,123 +11,113 @@ particlesJS.load('particles-js', 'particles.json', function() {
 
 /* Otherwise just put the config content (json): */
 
-particlesJS('particles-js',
-  
-  {
-    "particles": {
+particlesJS("particles-js", {
+  "particles": {
       "number": {
-        "value": 150,
-        "density": {
-          "enable": false,
-          "value_area": 800
-        }
+          "value": 180,
+          "density": {
+              "enable": true,
+              "value_area": 789.1476416322727
+          }
       },
       "color": {
-        "value": "#ffffff"
+          "value": "#ffffff"
       },
       "shape": {
-        "type": "circle",
-        "stroke": {
-          "width": 0,
-          "color": "#000000"
-        },
-        "polygon": {
-          "nb_sides": 5
-        },
-        "image": {
-          "src": "img/github.svg",
-          "width": 10,
-          "height": 10
-        }
+          "type": "circle",
+          "stroke": {
+              "width": 0,
+              "color": "#000000"
+          },
+          "polygon": {
+              "nb_sides": 3
+          },
+          "image": {
+              "src": "img/github.svg",
+              "width": 100,
+              "height": 100
+          }
       },
       "opacity": {
-        "value": 0.99,
-        "random": false,
-        "anim": {
-          "enable": true,
-          "speed": 1,
-          "opacity_min": 0,
-          "sync": false
-        }
+          "value": 1,
+          "random": true,
+          "anim": {
+              "enable": true,
+              "speed": 1,
+              "opacity_min": 0,
+              "sync": false
+          }
       },
       "size": {
-        "value": 5,
-        "random": true,
-        "anim": {
-          "enable": true,
-          "speed": 3,
-          "size_min": 0.1,
-          "sync": false
-        }
+          "value": 3,
+          "random": true,
+          "anim": {
+              "enable": false,
+              "speed": 4,
+              "size_min": 0.3,
+              "sync": false
+          }
       },
       "line_linked": {
-        "enable": false,
-        "distance": 150,
-        "color": "#ffffff",
-        "opacity": 0.4,
-        "width": 1
+          "enable": false,
+          "distance": 150,
+          "color": "#ffffff",
+          "opacity": 0.4,
+          "width": 1
       },
       "move": {
-        "enable": true,
-        "speed": 6,
-        "direction": "none",
-        "random": true,
-        "straight": false,
-        "out_mode": "out",
-        "attract": {
           "enable": true,
-          "rotateX": 600,
-          "rotateY": 1200
-        }
+          "speed": 2,
+          "direction": "none",
+          "random": true,
+          "straight": false,
+          "out_mode": "out",
+          "bounce": false,
+          "attract": {
+              "enable": false,
+              "rotateX": 600,
+              "rotateY": 600
+          }
       }
-    },
-    "interactivity": {
+  },
+  "interactivity": {
       "detect_on": "canvas",
       "events": {
-        "onhover": {
-          "enable": true,
-          "mode": "grab"
-        },
-        "onclick": {
-          "enable": true,
-          "mode": "repulse"
-        },
-        "resize": true
+          "onhover": {
+              "enable": true,
+              "mode": "grab"
+          },
+          "onclick": {
+              "enable": true,
+              "mode": "push"
+          },
+          "resize": true
       },
       "modes": {
-        "grab": {
-          "distance": 150,
-          "line_linked": {
-            "opacity": 1
+          "grab": {
+              "distance": 155.84415584415586,
+              "line_linked": {
+                  "opacity": 0.5423739790012226
+              }
+          },
+          "bubble": {
+              "distance": 95.90409590409591,
+              "size": 0,
+              "duration": 0.959040959040959,
+              "opacity": 0,
+              "speed": 3
+          },
+          "repulse": {
+              "distance": 103.89610389610391,
+              "duration": 0.4
+          },
+          "push": {
+              "particles_nb": 4
+          },
+          "remove": {
+              "particles_nb": 2
           }
-        },
-        "bubble": {
-          "distance": 100,
-          "size": 40,
-          "duration": 2,
-          "opacity": 8,
-          "speed": 3
-        },
-        "repulse": {
-          "distance": 400
-        },
-        "push": {
-          "particles_nb": 4
-        },
-        "remove": {
-          "particles_nb": 2
-        }
       }
-    },
-    "retina_detect": true,
-    "config_demo": {
-      "hide_card": false,
-      "background_color": "#b61924",
-      "background_image": "",
-      "background_position": "50% 50%",
-      "background_repeat": "no-repeat",
-      "background_size": "cover"
-    }
-  }
-
-);
+  },
+  "retina_detect": true
+});
