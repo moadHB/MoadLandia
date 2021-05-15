2da9b2e5c75d8153263ed8ae5b754f82242d988c
moadHB <moadhb@gmail.com>
2021-05-15 23:40:11 +0200

    form submit ajax, fetch api send mail

--- Content/js/site.js
+++ Content/js/site.js
@@ -2,37 +2,60 @@
 
 var movilEscondido = true;
 
-$("#lineas").css({ opacity: 0 });
+$(document).ready(function () {
+    $("#lineas").css({ opacity: 0 });
 
-//Mostrar menú
-$("#puntos").click(
-    function () {
-        if (movilEscondido) {
-            movilEscondido = false;
-            $("#puntos").animate({ opacity: 0 }, 150);
-            $("#lineas").css({ "z-index": 1 });
-            $("#lineas").animate({ opacity: 1 }, 150);
-            $(".dropdown-content-movil").css({ "max-height": "600px" });
-            $(".home-wrapper").hide();
+    //Mostrar menú
+    $("#puntos").click(
+        function () {
+            if (movilEscondido) {
+                movilEscondido = false;
+                $("#puntos").animate({ opacity: 0 }, 150);
+                $("#lineas").css({ "z-index": 1 });
+                $("#lineas").animate({ opacity: 1 }, 150);
+                $(".dropdown-content-movil").css({ "max-height": "600px" });
+                $(".home-wrapper").hide();
+            }
         }
-    }
-);
+    );
 
-//Esconder de nuevo
-$(document).click(function (e) {
-    var menu = $(".dropdown-content-movil");
-    var puntos = $("#puntos");
+    //Esconder de nuevo
+    $(document).click(function (e) {
+        var menu = $(".dropdown-content-movil");
+        var puntos = $("#puntos");
 
-    if (!menu.is(e.target) && menu.has(e.target).length === 0 && !puntos.is(e.target) && puntos.has(e.target).length === 0 && !movilEscondido) {
-        menu.css({ "max-height": "0vw" });
-        $("#puntos").animate({ opacity: 1 }, 150);
-        $("#lineas").css({ "z-index": -1 });
-        $("#lineas").animate({ opacity: 0 }, 150);
-        movilEscondido = true;
-        $(".home-wrapper").fadeIn(1000);
-    }
+        if (!menu.is(e.target) && menu.has(e.target).length === 0 && !puntos.is(e.target) && puntos.has(e.target).length === 0 && !movilEscondido) {
+            menu.css({ "max-height": "0vw" });
+            $("#puntos").animate({ opacity: 1 }, 150);
+            $("#lineas").css({ "z-index": -1 });
+            $("#lineas").animate({ opacity: 0 }, 150);
+            movilEscondido = true;
+            $(".home-wrapper").fadeIn(1000);
+        }
+    });
+
+    //PARALLAX
+    var scene = $('#scene').get(0);
+    var parallaxInstance = new Parallax(scene);
+
+    //Contact Form
+    var form = $("#my-form");
+    $("#my-form").submit(      
+        async function handleSubmit(event) {
+            event.preventDefault();
+            var status = $("#my-form-status");
+            var data = new FormData(event.target);
+            fetch(event.target.action, {
+            method: form.method,
+            body: data,
+            headers: {
+                'Accept': 'application/json'
+            }
+            }).then(response => {
+            status.innerHTML = "Thanks for your submission!";
+            form.reset()
+            }).catch(error => {
+            status.innerHTML = "Oops! There was a problem submitting your form"
+            });
+    });
 });
-
-//PARALLAX
-var scene = $('#scene').get(0);
-var parallaxInstance = new Parallax(scene);
